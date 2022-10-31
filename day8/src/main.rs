183249d272e2c9d647c857831862f9d2ac4e91cd
bigligands <jcrawfo@gmail.com>
2022-10-30 20:52:45 -0600

    solved day 8# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## Date:      Sun Oct 30 20:52:45 2022 -0600## On branch master# Your branch is up to date with 'origin/master'.## Changes to be committed:#	new file:   day8/Cargo.lock#	new file:   day8/Cargo.toml#	new file:   day8/input.txt#	new file:   day8/src/lib.rs#	new file:   day8/src/main.rs#	new file:   day8/test_input.txt## Changes not staged for commit:#	modified:   .gitignore#	modified:   day1/one/src/depths.txt#	modified:   day2/src/plannedcourse.txt#	modified:   day3/src/input.txt#	modified:   day4/src/input.txt#	modified:   day5/src/input.txt#	modified:   day6/src/input.txt#	modified:   day7/src/input.txt#	modified:   readme.md#

---
+++ day8/src/main.rs
@@ -0,0 +1,15 @@
+use day8::Display;
+use std::fs;
+
+fn main() {
+    //
+    let path = "input.txt";
+    let input = fs::read_to_string(path).unwrap();
+
+    let displays = day8::parse_input(&input[..]);
+    let mut sum = 0;
+    for display in displays {
+        sum += display.decipher_unique_outputs();
+    }
+    println!("Sum of unique outputs: {sum}");
+}
