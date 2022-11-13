a45f98ae3727289877f2c3f70056188e89db0310
bigligands <jcrawfo@gmail.com>
2022-11-13 15:52:36 -0700

    refactored for loops as iterators to help understand iterators.

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -5,59 +5,36 @@ fn main() {
     let input_path = "input.txt";
     let input = fs::read_to_string(input_path).unwrap();
 
-    let mut errors = Vec::new();
+    let a = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .scan(Vec::new(), |lastopen, c| match c {
+                    '(' | '[' | '{' | '<' => {
+                        lastopen.push(c);
+                        Some(0)
+                    }
+                    ')' => match lastopen.pop().unwrap() {
+                        '(' => Some(0),
+                        _ => Some(3),
+                    },
+                    ']' => match lastopen.pop().unwrap() {
+                        '[' => Some(0),
+                        _ => Some(57),
+                    },
+                    '}' => match lastopen.pop().unwrap() {
+                        '{' => Some(0),
+                        _ => Some(1197),
+                    },
+                    '>' => match lastopen.pop().unwrap() {
+                        '<' => Some(0),
+                        _ => Some(25137),
+                    },
+                    _ => Some(0),
+                })
+                .sum::<u32>()
+        })
+        .sum::<u32>();
 
-    for line in input.lines() {
-        let mut last_open = Vec::new();
-        let chars = line.chars().collect::<Vec<char>>();
-        for symbol in chars {
-            if symbol == '[' {
-                last_open.push('[')
-            } else if symbol == ']' {
-                match last_open.pop() {
-                    Some(c) => {
-                        if c != '[' {
-                            errors.push(57);
-                        }
-                    }
-                    None => errors.push(57),
-                }
-            } else if symbol == '(' {
-                last_open.push('(');
-            } else if symbol == ')' {
-                match last_open.pop() {
-                    Some(c) => {
-                        if c != '(' {
-                            errors.push(3);
-                        }
-                    }
-                    None => errors.push(3),
-                }
-            } else if symbol == '{' {
-                last_open.push('{');
-            } else if symbol == '}' {
-                match last_open.pop() {
-                    Some(c) => {
-                        if c != '{' {
-                            errors.push(1197);
-                        }
-                    }
-                    None => errors.push(1197),
-                }
-            } else if symbol == '<' {
-                last_open.push('<');
-            } else if symbol == '>' {
-                match last_open.pop() {
-                    Some(c) => {
-                        if c != '<' {
-                            errors.push(25137);
-                        }
-                    }
-                    None => errors.push(25137),
-                }
-            }
-        }
-    }
-    // println!("Errors: {:?}", errors);
-    println!("{}", errors.iter().sum::<u32>());
+    println!("{}", a);
 }
